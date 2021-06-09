8145bae1422622464e6784d129f7c66ba408b380
GoraChitich <78313253+GoraChitich@users.noreply.github.com>
2021-06-09 22:10:27 +0300

    added languages design from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Form from './components/Form/Form.js';
 import FormUpdate from './components/FormUpdate/FormUpdate.js';
 
 import { BrowserRouter as Route, Switch, HashRouter } from "react-router-dom"
 import DeleteOrder from './components/DeleteOrder/DeleteOrder';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
+function App() {
 
-  render() {
-     const baseName = process.env.PUBLIC_URL;
+  const baseName = process.env.PUBLIC_URL;
+  return (
+    <div className="App">
+      <HashRouter basename={baseName} >
+        <Switch>
+          <Route path="/" exact >
+            <Form />
+          </Route>
+          <Route path="/update/:id" exact>
+            <FormUpdate />
+          </Route>
+          <Route path="/delete/:id" exact>
+            <DeleteOrder />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </div>
 
-    return (
-      <div className="App">
-        <HashRouter basename={baseName} >
-          <Switch>
-            <Route path="/" exact >
-              <Form />
-            </Route>
-            <Route path="/update/:id" exact>
-              <FormUpdate />
-            </Route>
-            <Route path="/delete/:id" exact>
-              <DeleteOrder />
-            </Route>
-          </Switch>
-        </HashRouter>
-      </div>
-
-    );
-  }
+  );
 }
 
 export default App;
